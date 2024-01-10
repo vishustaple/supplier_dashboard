bcf4f3806a7a618c615000be6f6a6ffae3534829
ankittesttest <ankit@staplelogic.in>
2024-01-10 11:27:11 +0530

    worked on get column of excel sheet

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -35,39 +35,51 @@ class ExcelImportController extends Controller
             [
                 'supplierselect.required' => 'Please select a supplier. It is a required field.',
             ]
-          );
-          if( $validator->fails() )
-          {  
+        );
+
+        if( $validator->fails() ){  
             $categorySuppliers = CategorySupplier::all();
-              return view('admin.export',compact('categorySuppliers'))->withErrors($validator); 
-          }
-
-          $reader = new Xlsx(); 
-          $spreadsheet = $reader->load($request->file('file')); 
-          $worksheet = $spreadsheet->getActiveSheet();  
-          dd($worksheet);
-          $worksheet_arr = $worksheet->toArray(); 
-        // Get the uploaded file
-        $file = $request->file('file');
-
-        // Generate a timestamp to append to the file name
-        $timestamp = now()->format('YmdHis');
-
-        // Append timestamp to the file name
-        $fileName = $timestamp . '_' . $file->getClientOriginalName();
-
-        // Define the folder where you want to save the file
-        $destinationPath = public_path('/excel_sheets');
-
-      
-        // Move the file with the new name
-        $file->move($destinationPath, $fileName);
-
+            return view('admin.export',compact('categorySuppliers'))->withErrors($validator); 
+        }
         
-        /** Handle excel headers for validation 
-         * YourImportClass($supplier_id, $fileName, $destinationPath) this are the parameters
-        */
-        Excel::import(new YourImportClass($request->supplierselect, $fileName, $destinationPath), $destinationPath . '/' . $fileName);
+        $reader = new Xlsx(); 
+        $spreadsheet = $reader->load($request->file('file')); 
+        $worksheet = $spreadsheet->getActiveSheet();
+        
+        // Get the highest row and column numbers
+        $highestRow = $worksheet->getHighestRow();
+        $highestColumn = $worksheet->getHighestColumn();
+        
+        // Convert the column letter to a number
+        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn) - 1;
+    
+        // Variables to store information about the row with the highest number of columns
+        $worksheet_arr = $worksheet->toArray(); 
+
+        $start_index_value_array = $value_array_key = $maxNonEmptyCount = 0;
+        foreach ($worksheet_arr as $key=>$value) {
+            //Checking not empty columns
+            $nonEmptyCount = count(array_filter(array_values($value), function ($item) {
+                return !empty($item);
+            }));
+            
+            // if column count is greater then previous row columns count. Then assigen value to '$maxNonEmptyvalue'
+            if ($nonEmptyCount == $highestColumnIndex) {
+                $maxNonEmptyCount = $nonEmptyCount;
+                $maxNonEmptyvalue = $value;
+                $start_index_value_array = $key;
+                break;
+            }
+        }
+
+            /** Remove empty key from the array of excel sheet column name */
+            $finalExcelKeyArray = array_values(array_filter($maxNonEmptyvalue, function ($item) {
+                return !empty($item);
+            }, ARRAY_FILTER_USE_BOTH));
+            echo"<pre>";
+            print_r($finalExcelKeyArray);
+            die;
+     
 
     return redirect()->back()->with('success', 'Excel file imported successfully!');
 
