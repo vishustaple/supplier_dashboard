7bb4fa7212cb454607a7292df0dde099228c8870
ankittesttest <ankit@staplelogic.in>
2024-01-17 17:45:42 +0530

    worked on cron function some condition changes

--- app/Console/Commands/MailSend.php
+++ app/Console/Commands/MailSend.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Carbon\Carbon;
-use App\Models\{UploadedFiles,CategorySupplier};
+use App\Models\{UploadedFiles, CategorySupplier};
 use Illuminate\Support\Facades\Mail;
 class MailSend extends Command
 {
@@ -25,54 +25,47 @@ class MailSend extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
-    {
-        $supplierid=['1','2','3','4','5','6'];
-        foreach($supplierid as $id){
-        
-           if($id == 5){
+    public function handle(){
+        $supplierId = ['1','2','3','4','5','6'];
 
-   
-            if (Carbon::now()->day <= 15) {
-                // First half of the month
-                $startDate = Carbon::now()->startOfMonth();
-                $endDate = Carbon::now()->startOfMonth()->addDays(14)->endOfDay();
-            
-                if(!empty($startDate) && !empty($endDate)){
-                        
-                    $data=UploadedFiles::where('supplier_id',$id)->whereBetween('created_at', [$startDate, $endDate])->latest('created_at')
-                    ->first();
-                  
-                    if(!isset($data)){
-                        $this->info('no issue first half ' . $id);
-                      
-                            $supplierName=CategorySupplier::where('id',$id)->value('supplier_name');
-                            $email='vishustaple.in@gmail.com'; 
+        foreach($supplierId as $id){
+            if($id == 5){
+                if (Carbon::now()->day <= 15) {
+                    /** First half of the month */
+                    $startDate = Carbon::now()->startOfMonth();
+                    $endDate = Carbon::now()->startOfMonth()->addDays(14)->endOfDay();
+                
+                    if(!empty($startDate) && !empty($endDate)){
+                        $data = UploadedFiles::where('supplier_id',$id)->whereBetween('created_at', [$startDate, $endDate])->latest('created_at')
+                        ->first();
+                    
+                        if(!isset($data)){
+                            $this->info('no issue first half ' . $id);
+
+                            $supplierName = CategorySupplier::where('id',$id)->value('supplier_name');
+                            $email = 'vishustaple.in@gmail.com';
+
                             $this->sendEmail($email, $supplierName);
                         }
-                }
-               
-            } else {
-                // Second half of the month
-                $startDate = Carbon::now()->startOfMonth()->addDays(15);
-                $endDate = Carbon::now()->endOfMonth();
-                if(!empty($startDate) && !empty($endDate)){
+                    }
+                } else {
+                    /** Second half of the month */
+                    $startDate = Carbon::now()->startOfMonth()->addDays(15);
+                    $endDate = Carbon::now()->endOfMonth();
+                    if(!empty($startDate) && !empty($endDate)){
+                            
+                        $data = UploadedFiles::where('supplier_id', $id)->whereBetween('created_at', [$startDate, $endDate])->latest('created_at')
+                        ->first();
+                        if(!isset($data)){
+                            $this->info('no issue second half ' . $id);
                         
-                    $data=UploadedFiles::where('supplier_id',$id)->whereBetween('created_at', [$startDate, $endDate])->latest('created_at')
-                    ->first();
-                    if(!isset($data)){
-                        $this->info('no issue second half ' . $id);
-                      
-                            $supplierName=CategorySupplier::where('id',$id)->value('supplier_name');
-                            $email='vishustaple.in@gmail.com'; 
-                            $this->sendEmail($email, $supplierName);
-                        }
+                                $supplierName = CategorySupplier::where('id',$id)->value('supplier_name');
+                                $email = 'vishustaple.in@gmail.com'; 
+                                $this->sendEmail($email, $supplierName);
+                            }
+                    }
                 }
             }
-            
-   
-
-           }
 
 
             if($id == 1 || $id == 2 || $id == 3|| $id == 4){
@@ -82,7 +75,7 @@ class MailSend extends Command
                     // dd($currentDay);
                     $currentWeek = ceil($currentDay / 7); 
                     if ($currentWeek % 2 == 1) {
-                        // Odd week
+                        /** Odd week */
                         $startDate = Carbon::now()->startOfWeek();
                         $endDate = Carbon::now()->endOfWeek();
                 
@@ -97,11 +90,13 @@ class MailSend extends Command
                 
                                 $supplierName = CategorySupplier::where('id', $id)->value('supplier_name');
                                 $email = 'vishustaple.in@gmail.com';
+
                                 $this->sendEmail($email, $supplierName);
                             }
                         }
                     } else {
-                        // Even week
+
+                        /** Even week */
                         $startDate = Carbon::now()->startOfWeek()->addDays(7);
                         $endDate = Carbon::now()->endOfWeek()->addDays(7);
                 
@@ -123,38 +118,40 @@ class MailSend extends Command
 
                 }
 
-            // Get the current month start date
-            $startDate = Carbon::now()->startOfMonth();
-            $endDate = Carbon::now()->endOfMonth();
-                         if(!empty($startDate) && !empty($endDate)){
+                /** Get the current month start date */
+                $startDate = Carbon::now()->startOfMonth();
+                $endDate = Carbon::now()->endOfMonth();
+
+                if(!empty($startDate) && !empty($endDate)){
+                    $data=UploadedFiles::where('supplier_id',$id)->whereBetween('created_at', [$startDate, $endDate])->latest('created_at')
+                    ->first();
+
+                    if(!isset($data)){
+                        $this->info('no issue  ' . $id);
+
+                        $supplierName = CategorySupplier::where('id',$id)->value('supplier_name');
+                        $email = 'vishustaple.in@gmail.com';
                         
-                        $data=UploadedFiles::where('supplier_id',$id)->whereBetween('created_at', [$startDate, $endDate])->latest('created_at')
-                        ->first();
-                        if(!isset($data)){
-                            $this->info('no issue  ' . $id);
-                          
-                                $supplierName=CategorySupplier::where('id',$id)->value('supplier_name');
-                                $email='vishustaple.in@gmail.com'; 
-                                $this->sendEmail($email, $supplierName);
-                            }
+                        $this->sendEmail($email, $supplierName);
                     }
                 }
+            }
         }
-     
     }
-      // Another function
-      protected function sendEmail($email, $supplierName)
-      {
+
+    /** Another function */
+    protected function sendEmail($email, $supplierName){
         try {
             Mail::send('mail.pendingfile', ['suppliername' => $supplierName], function ($m) use ($email) {
                 $m->from($email, 'Supplier Admin');
                 $m->to('supplieradmin@gmail.com')->subject('Pending Files else');
             });
-    
+
             $this->info('Email sent successfully');
+            
         } catch (\Exception $e) {
-            // Handle the exception here
+            /** Handle the exception here */
             $this->error('Email sending failed: ' . $e->getMessage());
         }
-      }
+    }
 }
