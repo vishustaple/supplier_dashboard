92f6d28a9ef7d204870ab5604f0d96c2f07ab11a
ankittesttest <ankit@staplelogic.in>
2024-01-29 16:06:40 +0530

    worked on record type table migration

---
+++ database/migrations/2023_01_05_061405_create_record_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('record_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('record_type_name', 255);
+            $table->unsignedBigInteger('created_by');
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users'); /** Assuming 'users' table exists */
+            /** Add other foreign key constraints if needed */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Schema::dropIfExists('record_types');
+    }
+};
