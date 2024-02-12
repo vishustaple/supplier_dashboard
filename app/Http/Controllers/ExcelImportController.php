48286298763e5c5ff4e50bfc1387713817acb20b
ankittesttest <ankit@staplelogic.in>
2024-02-12 18:23:25 +0530

    worked on changes

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -46,7 +46,12 @@ class ExcelImportController extends Controller
                 $item->createdByUser->first_name.' '.$item->createdByUser->last_name,
                 (isset($item->deletedByUser->id) && !empty($item->deletedByUser->id)) ? ($item->deletedByUser->first_name.' '.$item->deletedByUser->last_name) : (''),
                 $item->created_at->format('m/d/Y'),
-                (isset($item->deleted_at) && !empty($item->deleted_at) ? '<button class="btn btn-danger btn-xs remove invisible" ><i class="fa-solid fa-trash"></i></button>' : '<button data-id="'.$item->id.'" class="btn btn-danger btn-xs remove" title="Remove File"><i class="fa-solid fa-trash"></i></button>'),
+                (isset($item->delete) && !empty($item->delete)) ? ('<div class="spinner">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
+              </div>
+              ') : ((isset($item->deleted_at) && !empty($item->deleted_at) ? '<button class="btn btn-danger btn-xs remove invisible" ><i class="fa-solid fa-trash"></i></button>' : '<button data-id="'.$item->id.'" class="btn btn-danger btn-xs remove" title="Remove File"><i class="fa-solid fa-trash"></i></button>')),
                 // $item->updated_at->format('m/d/Y'),
             ];
             $i++;
