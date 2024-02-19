4bf8f7203396846cb285204162c08705401c261b
ankittesttest <ankit@staplelogic.in>
2024-02-19 19:00:07 +0530

    worked on migration

--- app/Console/Commands/ProcessUploadedSupplierCatelogFiles.php
+++ app/Console/Commands/ProcessUploadedSupplierCatelogFiles.php
@@ -28,61 +28,65 @@ class ProcessUploadedSupplierCatelogFiles extends Command
      */
     public function handle()
     {
-        $supplierId = [3, 4, 5];
-        for ($i=0; $i <=count($supplierId) ; $i++) {
-            $curruentSupplierId = $supplierId[$i]; 
-            $catelogTableKeyArray = [
-                3 =>[
-                    'UM' => 'um',
-                    'WBE' => 'wbe',
-                    'MBE' => 'mbe',
-                    'Sugg' => 'sugg',
-                    'Item' => 'item', 
-                    'Recycled' => 'recycled',
-                    'Sku Number' => 'sku_number',
-                    'Vendor Prd#' => 'vendor_prd',
-                    'Vendor Name' => 'vendor_name',
-                    'Platinum Price' => 'platinum_price',
-                    'Preferred Price' => 'preferred_price',
-                    'Dept Description' => 'dept_description',
-                    'Class Description' => 'class_description',
-                    'Preferred Price Method' => 'preferred_price_method',
-                    'Platinum Price Method' => 'platinum_price_method',
-                ],
+        // $supplierId = [3, 4, 5];
+        // $tableName = [3 => 'catalog_od', 4 =>'catalog_staples', 5 =>'catalog_wbm']; 
 
-                4 =>[
-                    'SELLUOM' => 'selluom',
-                    'SKUNUMBER' => 'sku_number',
-                    'PRODCLASS' => 'prod_class',
-                    'QTYINSELLUOM' => 'qty_in_selluom',
-                    'AVGSELLPRICE' => 'avg_sell_price',
-                    'PRIMARYPRODCAT' => 'primary_prod_cat',
-                    'STAPLESOWNBRAND' => 'staple_own_brand',
-                    'ITEMDESCRIPTION' => 'item_description',
-                    'SECONDARYPRODCAT' => 'secondary_prod_cat',
-                    'STAPLESADVANTAGEITEMDESCRIPTION' => 'staples_advantages_item_description',
-                ],
-                 
-                5 =>[
-                    'UOM' => 'uom',
-                    'WB QPU' => 'wb_qpu',  
-                    'FullSKU' => 'full_sku',
-                    'Category' => 'category',
-                    'Unit Price' => 'unit_price',
-                    'List Price' => 'list_price',
-                    'ProductCode' => 'product_code',
-                    'Manufacturer' => 'manufacturer',
-                    'ITEM DESCRIPTION' => 'item_description',
-                    'Category Umbrella' => 'category_umbrella',
-                ]
-            ];
+        $supplierId = [4, 5];
+        $tableName = [4 =>'catalog_staples', 5 =>'catalog_wbm']; 
+        $catelogTableKeyArray = [
+            3 =>[
+                'UM' => 'um',
+                'WBE' => 'wbe',
+                'MBE' => 'mbe',
+                'Sugg' => 'sugg',
+                'Item' => 'item', 
+                'Recycled' => 'recycled',
+                'Sku Number' => 'sku_number',
+                'Vendor Prd#' => 'vendor_prd',
+                'Vendor Name' => 'vendor_name',
+                'Platinum Price' => 'platinum_price',
+                'Preferred Price' => 'preferred_price',
+                'Dept Description' => 'dept_description',
+                'Class Description' => 'class_description',
+                'Preferred Price Method' => 'preferred_price_method',
+                'Platinum Price Method' => 'platinum_price_method',
+            ],
+
+            4 =>[
+                'SELLUOM' => 'selluom',
+                'SKUNUMBER' => 'sku_number',
+                'PRODCLASS' => 'prod_class',
+                'QTYINSELLUOM' => 'qty_in_selluom',
+                'AVGSELLPRICE' => 'avg_sell_price',
+                'PRIMARYPRODCAT' => 'primary_prod_cat',
+                'STAPLESOWNBRAND' => 'staple_own_brand',
+                'ITEMDESCRIPTION' => 'item_description',
+                'SECONDARYPRODCAT' => 'secondary_prod_cat',
+                'STAPLESADVANTAGEITEMDESCRIPTION' => 'staples_advantages_item_description',
+            ],
+             
+            5 =>[
+                'UOM' => 'uom',
+                'WB QPU' => 'wb_qpu',  
+                'FullSKU' => 'full_sku',
+                'Category' => 'category',
+                'Unit Price' => 'unit_price',
+                'List Price' => 'list_price',
+                'ProductCode' => 'product_code',
+                'Manufacturer' => 'manufacturer',
+                'ITEM DESCRIPTION' => 'item_description',
+                'Category Umbrella' => 'category_umbrella',
+            ]
+        ];
+        for ($i=0; $i <=count($supplierId) ; $i++) {
+            $curruentSupplierId = $supplierId[$i];
             $query = CatalogDetail::query()
             ->leftJoin('catalog', 'catalog.id', '=', 'catalog_details.catalog_id')
             ->select('catalog_details.catalog_id as id', 'catalog_details.table_key as table_key', 'catalog_details.table_value as table_value');
             $query->where('catalog.supplier_id', '=', $curruentSupplierId);
     
             $chunkSize = 10000;
-            $query->chunk($chunkSize, function ($catalogDetails)  use ($catelogTableKeyArray, $curruentSupplierId) {
+            $query->chunk($chunkSize, function ($catalogDetails)  use ($catelogTableKeyArray, $curruentSupplierId, $tableName) {
                 // Process each chunk of catalog details here
                 foreach ($catalogDetails->toArray() as $catalogDetail) {
                     $formatuserdata[$catalogDetail['id']][] = [
@@ -101,7 +105,7 @@ class ProcessUploadedSupplierCatelogFiles extends Command
                     }
                 }
     
-                DB::table('catalog_staples')->insert($finalArray);  
+                DB::table($tableName[$curruentSupplierId])->insert($finalArray);  
                 unset($finalArray);
             });
         }
