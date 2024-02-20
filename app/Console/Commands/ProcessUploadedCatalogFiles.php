f23032c15725fb54bca9ffafd028c88edbb0e467
ankittesttest <ankit@staplelogic.in>
2024-02-20 18:02:10 +0530

    worked on changes

--- app/Console/Commands/ProcessUploadedCatalogFiles.php
+++ app/Console/Commands/ProcessUploadedCatalogFiles.php
@@ -35,16 +35,17 @@ class ProcessUploadedCatalogFiles extends Command
         
         /** This is the folder path where we save the file */
         $destinationPath = public_path('/excel_sheets');
-        ini_set('memory_limit', '19024M');
+        ini_set('memory_limit', '1024M');
         // print_r($fileValue->created_by);die;
         $reader = new Xlsx(); /** Creating object of php excel library class */
 
+     
         $supplierId = 3;
         $fileNameArray = [
-            // 3 => ['odCatelog1.xlsx'],
+            3 => ['odCatelog1.xlsx'],
             // 3 => ['odCatelog2.xlsx'],
             // 3 => ['odCatelog4.xlsx',
-            3 => ['odCatelog3.xlsx'],
+            // 3 => ['odCatelog3.xlsx'],
             // 3 => ['odCatelog6.xlsx'],
             // 3 => ['odCatelog8.xlsx'],
             // 3 => ['odCatelog10.xlsx'], 
@@ -62,7 +63,7 @@ class ProcessUploadedCatalogFiles extends Command
             // 3 => ['odCatelog23.xlsx'],
             // 3 => ['odCatelogk24.xlsx'],
             // 5 => ['CatalogWBM.xlsx'], 
-            4 => ['CatelogStaples.xlsx']
+            // 4 => ['CatelogStaples.xlsx']
         ];
         for ($i=0; $i < count($fileNameArray[$supplierId]); $i++) { 
             /** Loading excel file using path and name of file from table "uploaded_file" */
@@ -112,90 +113,34 @@ class ProcessUploadedCatalogFiles extends Command
                         if (empty($row[0]) && empty($row[4]) && empty($row[8])) {
                             continue;
                         } else {
-                            $catalogLastInsertId = Catalog::create([
-                                'sku' => (isset($row[0]) && !empty($row[0])) ? ($row[0]) : (''),
-                                'price' => (isset($row[4]) && !empty($row[4])) ? ($row[4]) : (''),
-                                'created_by' => 1,
-                                'supplier_id' => $supplierId,
-                                'description' => (isset($row[8]) && !empty($row[8])) ? ($row[8]) : ('')
-                            ]);
+                            $finalInsertArray[] = [   
+                            'um' => $row[1],
+                            'wbe' => $row[13],
+                            'mbe' => $row[12],
+                            'sugg' => $row[10],
+                            'item' => $row[3],
+                            'recycled' => $row[14],
+                            'sku_number' => $row[0],
+                            'vendor_prd' => $row[2],
+                            'vendor_name' => $row[11],
+                            'platinum_price' => $row[4],
+                            'preferred_price' => $row[6],
+                            'dept_description' => $row[8],
+                            'class_description' => $row[9],
+                            'preferred_price_method' => $row[7],
+                            'platinum_price_method' => $row[5],
+                            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                            ];
 
-                            foreach ($row as $key1 => $value) {
-                                if(!empty($maxNonEmptyValue[$key1])) {     
-                                    $finalInsertArray[] = [
-                                        'table_value' => $value,
-                                        'created_by' => 1,
-                                        'table_key' => $maxNonEmptyValue[$key1],
-                                        'catalog_id' => $catalogLastInsertId->id,
-                                        'file_name' => 'Account Info.xlsx',
-                                        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                                        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                                    ];  
-                                }
-                            }
+                           
                         }
-                    } elseif ($supplierId == 4) {
-                        if (empty($row[0]) && empty($row[5]) && empty($row[1])) {
-                            continue;
-                        } else {
-                            $catalogLastInsertId = Catalog::create([
-                                'sku' => (isset($row[0]) && !empty($row[0])) ? ($row[0]) : (''),
-                                'price' => (isset($row[5]) && !empty($row[5])) ? ($row[5]) : (''),
-                                'created_by' => 1,
-                                'supplier_id' => $supplierId,
-                                'description' => (isset($row[1]) && !empty($row[1])) ? ($row[1]) : ('')
-                            ]);
-
-                            foreach ($row as $key1 => $value) {
-                                if(!empty($maxNonEmptyValue[$key1])) {     
-                                    $finalInsertArray[] = [
-                                        'table_value' => $value,
-                                        'created_by' => 1,
-                                        'table_key' => $maxNonEmptyValue[$key1],
-                                        'catalog_id' => $catalogLastInsertId->id,
-                                        'file_name' => 'Account Info.xlsx',
-                                        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                                        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                                    ];  
-                                }
-                            }
-                        }
-                    } elseif ($supplierId == 5) {
-                        if (empty($row[0]) && empty($row[5]) && empty($row[3])) {
-                            continue;
-                        } else {
-                            $catalogLastInsertId = Catalog::create([
-                                'sku' => (isset($row[0]) && !empty($row[0])) ? ($row[0]) : (''),
-                                'price' => (isset($row[5]) && !empty($row[5])) ? ($row[5]) : (''),
-                                'created_by' => 1,
-                                'supplier_id' => $supplierId,
-                                'description' => (isset($row[3]) && !empty($row[3])) ? ($row[3]) : ('')
-                            ]);
-
-                            foreach ($row as $key1 => $value) {
-                                if(!empty($maxNonEmptyValue[$key1])) {     
-                                    $finalInsertArray[] = [
-                                        'table_value' => $value,
-                                        'created_by' => 1,
-                                        'table_key' => $maxNonEmptyValue[$key1],
-                                        'catalog_id' => $catalogLastInsertId->id,
-                                        'file_name' => 'Account Info.xlsx',
-                                        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                                        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                                    ];  
-                                }
-                            }
-                        }
-                    } else {
-    
                     }
-                    
-                    
-    
-                    if ($count == 100) {
+
+                    if ($count == 1000) {
                         $count = 0;
                         try {
-                            DB::table('catalog_details')->insert($finalInsertArray);
+                            DB::table('catalog_od')->insert($finalInsertArray);
                         } catch (QueryException $e) {   
                             Log::error('Error in YourScheduledTask: ' . $e->getMessage());
                             echo "Database insertion failed: " . $e->getMessage();
@@ -208,7 +153,7 @@ class ProcessUploadedCatalogFiles extends Command
                 }
                 if (!empty($finalInsertArray)) {
                     try {
-                        DB::table('catalog_details')->insert($finalInsertArray);
+                        DB::table('catalog_od')->insert($finalInsertArray);
                     } catch (QueryException $e) {   
                         Log::error('Error in YourScheduledTask: ' . $e->getMessage());
                         echo "Database insertion failed: " . $e->getMessage();
