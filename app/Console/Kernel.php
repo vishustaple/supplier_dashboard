40545a05349a8250491b9a23cce8dd984362ea36
ankittesttest <ankit@staplelogic.in>
2024-02-21 12:32:09 +0530

    worked on corn timing change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command(MailSend::class)->everyMinute();
-        $schedule->command(ProcessUploadedFiles::class)->everyTenMinutes();
+        $schedule->command(ProcessUploadedFiles::class)->everyTwoMinutes();
         $schedule->command(DeleteUploadedFilesData::class)->everyFiveMinutes();   
     }
 
