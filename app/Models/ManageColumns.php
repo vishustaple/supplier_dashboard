08b777424fd1b30511e3a375c5ad952ff23375b4
vishustaple <vishustaple.in@gmail.com>
2024-02-22 15:31:43 +0530

    make extra remove function

--- app/Models/ManageColumns.php
+++ app/Models/ManageColumns.php
@@ -9,4 +9,48 @@ class ManageColumns extends Model
 {
     use HasFactory;
     protected $table = 'manage_columns';
+    protected $fillable = [
+        'supplier_id',
+        'required',
+        'field_name',
+    ];
+    public static function getColumns(){
+        $supplierIDsGrouped = ManageColumns::groupBy('supplier_id')->pluck('supplier_id')->toArray();
+
+        $allSupplierData = [];
+        foreach ($supplierIDsGrouped as $supplier) {
+            $columns = ManageColumns::where('required', '1')
+                ->where('supplier_id', $supplier)
+                ->pluck('field_name')
+                ->toArray();
+            $allSupplierData[$supplier] = $columns;
+        }
+        
+        $jsArray = '[';
+        
+        foreach ($allSupplierData as $supplierId => $subArray) {
+            if (!empty($subArray)) {
+                // Remove single quotes from each string element
+                $cleanedSubArray = array_map(function ($item) {
+                    return str_replace("'", "", $item);
+                }, $subArray);
+                $jsArray .= "'$supplierId' => ['" . implode("', '", $subArray) . "'],";
+            } else {
+                $jsArray .= "'$supplierId' => [],";
+            }
+        }
+        
+        $jsArray .= ']';
+        return $jsArray;
+    }
+    public static function cleanRows(array $array){
+        foreach ($array as &$row) {
+            $row = str_replace(' ', '', $row);
+            $row = preg_replace('/[^\w\s]/', '', $row);
+            $row = strtolower($row);
+        }
+        return $array;
+
+    }
+    
 }
