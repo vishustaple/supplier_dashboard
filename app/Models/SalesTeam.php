18bf347ee267b61ccb6f51fbbdd1caa3e9b5a835
vishustaple <vishustaple.in@gmail.com>
2024-02-26 11:39:12 +0530

    working on salesteam add operation

--- app/Models/SalesTeam.php
+++ app/Models/SalesTeam.php
@@ -10,6 +10,13 @@ class SalesTeam extends Model
     use HasFactory;
     
     protected $table = 'sales_team';
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'status',
+      ];
     public static function getFilterdSalesData($filter=[], $csv=false){
         $orderColumnArray = [
             0 => 'sales_team.first_name',
