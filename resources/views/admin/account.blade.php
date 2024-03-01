af2d42d799aa43d90c10e18ad626e7817e4e49a8
vishustaple <vishustaple.in@gmail.com>
2024-03-01 17:48:57 +0530

    remove edit and account from account field

--- resources/views/admin/account.blade.php
+++ resources/views/admin/account.blade.php
@@ -13,7 +13,7 @@
                 <!-- Button trigger modal -->
                 <a href="{{ route('account.create')}}" class="btn btn-primary">
                 <i class="fa-solid fa-plus"></i> Account</a>
-                <button id="downloadAccountCsvBtn" class="btn-success btn" title="Csv Download"><i class="fa-solid me-2 fa-file-csv"></i>Download</button>
+                <!-- <button id="downloadAccountCsvBtn" class="btn-success btn" title="Csv Download"><i class="fa-solid me-2 fa-file-csv"></i>Download</button> -->
                     <a href="{{ route('account.customer-edit')}}" class="bell_icon_link btn btn-info position-relative">
                        <i class="fa-solid fa-bell"></i>
                         @if($totalmissingaccount > 0)
