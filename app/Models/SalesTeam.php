228b2ec738a0fe6612150dc517bc49b16a035b6a
vishustaple <vishustaple.in@gmail.com>
2024-03-06 18:36:11 +0530

    sales changes

--- app/Models/SalesTeam.php
+++ app/Models/SalesTeam.php
@@ -17,7 +17,7 @@ class SalesTeam extends Model
         'status',
         'last_name',
         'first_name',
-        'team_user_type'
+        'team_user_type',
     ];
     const STATUS_ACTIVE = 1;
     const STATUS_INACTIVE = 0;
@@ -30,6 +30,7 @@ class SalesTeam extends Model
             1 => 'sales_team.email',
             2 => 'sales_team.phone',
             3 => 'sales_team.status',
+            4 => 'sales_team.team_user_type',
         ];
 
         $query = self::query() // Replace YourModel with the actual model you are using for the data
@@ -40,6 +41,7 @@ class SalesTeam extends Model
             'sales_team.email as email' ,
             'sales_team.phone as phone',
             'sales_team.status as status' ,
+            'sales_team.team_user_type as team_user_type',
         ); // Adjust the column names as needed
 
         // Search functionality
@@ -86,8 +88,10 @@ class SalesTeam extends Model
             $formatuserdata[$key]['name'] = $data->first_name.' '.$data->last_name;
             $formatuserdata[$key]['email'] = $data->email;
             $formatuserdata[$key]['phone'] = $data->phone;
+           
             // $formatuserdata[$key]['status'] = ($data->status == 1) ? ("Active") : ("In-Active");    
-            $formatuserdata[$key]['action'] = '<div class="dropdown custom_drop_down"><a class="dots" href="#" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-ellipsis-vertical"></i></a> <div class="dropdown-menu"><a class=" " title="View Details" href= '.route('sales.index', ['id' => $data->id]).'><i class="fa-regular  fa-eye"></i>View</a> <a title="Edit SalesTeam" class=" " href= '.route('sales.edit', ['id' => $data->id,'routename' => 'sales']).' ><i class="fa-regular fa-pen-to-square"></i>Edit</a></div></div>';
+            $formatuserdata[$key]['action'] = '<div class="dropdown custom_drop_down"><a class="dots" href="#" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-ellipsis-vertical"></i></a> <div class="dropdown-menu"> <a title="Edit SalesTeam" class=" " href= '.route('sales.edit', ['id' => $data->id,'routename' => 'sales']).' ><i class="fa-regular fa-pen-to-square"></i>Edit</a></div></div>';
+            // <a class=" " title="View Details" href= '.route('sales.index', ['id' => $data->id]).'><i class="fa-regular  fa-eye"></i>View</a>
             // <a hrefe="#" data-id="'. $data->id .'" class="remove" title="Remove Sales"><i class="fa-solid fa-trash"></i>Remove</a>
             $formatuserdata[$key]['status'] = '<div class="form-check form-switch">
             <input class="form-check-input m-0" type="checkbox" role="switch" id="flexSwitchCheckChecked"';
@@ -97,6 +101,16 @@ class SalesTeam extends Model
             // Add onclick event handler
             $formatuserdata[$key]['status'] .= ' onclick="toggleDisableEnable('.$data->id.')"';
             $formatuserdata[$key]['status'] .= '></div>';
+            $userTypeLabels = [
+                1 => 'Sales',
+                2 => 'Agent',
+                3 => 'Customer Services',
+             
+            ];
+            $userType = isset($userTypeLabels[$data->team_user_type]) ? $userTypeLabels[$data->team_user_type] : 'Unknown';
+
+            $formatuserdata[$key]['team_user_type'] = $userType;
+            
 
         }
         
