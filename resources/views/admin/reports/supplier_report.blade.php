9208af16d60d83f7ecc64be523354ae9bf869af9
ankittesttest <ankit@staplelogic.in>
2024-03-19 13:07:21 +0530

    worked on changes

--- resources/views/admin/reports/supplier_report.blade.php
+++ resources/views/admin/reports/supplier_report.blade.php
@@ -131,7 +131,7 @@
             oLanguage: {sProcessing: '<div id="page-loader"><div id="page-loader-wrap"><div class="spinner-grow text-primary" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-success" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-danger" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-warning" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-info" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-light" role="status"><span class="sr-only">Loading...</span></div></div></div>'},
             processing: true,
             serverSide: true,
-            // lengthMenu: [],
+            lengthMenu: false,
             // paging: false,
             searching:false, 
             pageLength: 40,
