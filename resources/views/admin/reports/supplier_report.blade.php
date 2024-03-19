4c155379288552b42a94a36e9468b7a3bf37da13
ankittesttest <ankit@staplelogic.in>
2024-03-19 13:13:38 +0530

    worked on changes

--- resources/views/admin/reports/supplier_report.blade.php
+++ resources/views/admin/reports/supplier_report.blade.php
@@ -131,7 +131,8 @@
             oLanguage: {sProcessing: '<div id="page-loader"><div id="page-loader-wrap"><div class="spinner-grow text-primary" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-success" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-danger" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-warning" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-info" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-light" role="status"><span class="sr-only">Loading...</span></div></div></div>'},
             processing: true,
             serverSide: true,
-            lengthMenu: false,
+            lengthMenu: [40], // Specify the options you want to show
+            lengthChange: false, // Hide the "Show X entries" dropdown
             // paging: false,
             searching:false, 
             pageLength: 40,
