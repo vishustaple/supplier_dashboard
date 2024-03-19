4a115b7e0c5b0e5c5da3e768ab602274f1848502
ankittesttest <ankit@staplelogic.in>
2024-03-19 18:22:15 +0530

    worked on business report

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -215,10 +215,10 @@ class Order extends Model
                 $finalArray[$key]['supplier'] = $value->supplier_name;
                 $finalArray[$key]['account_name'] = $value->account_name;
                 $finalArray[$key]['amount'] = '$'.$value->amount;
-                // $finalArray[$key]['volume_rebate'] = ($value->amount/100)*$value->volume_rebate;
-                // $finalArray[$key]['incentive_rebate'] = ($value->amount/100)*$value->incentive_rebate;
-                $finalArray[$key]['volume_rebate'] = '<input type="hidden" value="'.$totalVolumeRebate.'"class="input_volume_rebate">'.(!empty($value->volume_rebate) ? ($value->volume_rebate.'%') : (''));
-                $finalArray[$key]['incentive_rebate'] = '<input type="hidden" value="'.$totalIncentiveRebate.'" class="input_incentive_rebate">'.((!empty($value->incentive_rebate)) ? ($value->incentive_rebate.'%') : (''));
+                $finalArray[$key]['volume_rebate'] = ($value->amount/100)*$value->volume_rebate;
+                $finalArray[$key]['incentive_rebate'] = ($value->amount/100)*$value->incentive_rebate;
+                // $finalArray[$key]['volume_rebate'] = '<input type="hidden" value="'.$totalVolumeRebate.'"class="input_volume_rebate">'.(!empty($value->volume_rebate) ? ($value->volume_rebate.'%') : (''));
+                // $finalArray[$key]['incentive_rebate'] = '<input type="hidden" value="'.$totalIncentiveRebate.'" class="input_incentive_rebate">'.((!empty($value->incentive_rebate)) ? ($value->incentive_rebate.'%') : (''));
                 $finalArray[$key]['date'] = date_format(date_create($value->date), 'm/d/Y');
                 // $finalArray[$key]['start_date'] = date_format(date_create($filter['start_date']), 'Y-m-d H:i:s');
                 // $finalArray[$key]['end_date'] = date_format(date_create($filter['end_date']), 'Y-m-d H:i:s');
