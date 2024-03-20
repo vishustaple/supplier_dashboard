a99ebb4f3a341d83a4709c43954dddfdb9a41735
ankittesttest <ankit@staplelogic.in>
2024-03-20 12:40:40 +0530

    worked on changes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -196,8 +196,9 @@ class Order extends Model
 
         
 
-        $totalVolumeRebate = $query->sum(DB::raw('`orders`.`amount` / 100) * `rebate`.`volume_rebate`'));
-        $totalIncentiveRebate = $query->sum(DB::raw('`orders`.`amount` / 100) * `rebate`.`incentive_rebate`'));
+        $totalVolumeRebate = $query->sum(DB::raw('(`orders`.`amount` / 100) * `rebate`.`volume_rebate`'));
+        $totalIncentiveRebate = $query->sum(DB::raw('(`orders`.`amount` / 100) * `rebate`.`incentive_rebate`'));
+
         // $totalVolumeRebate = $totalIncentiveRebate = 0;
         // dd($query->get());
         // foreach ($query->get() as $key => $value) {
