b491907fdc3231c7fbdbad303549d2c22d1872fe
ankittesttest <ankit@staplelogic.in>
2024-04-04 11:34:51 +0530

    worked on some changes

--- resources/views/admin/supplier.blade.php
+++ resources/views/admin/supplier.blade.php
@@ -14,64 +14,63 @@
         </div>
     </div>
     <div class="modal fade" id="editSupplierModal" tabindex="-1" aria-labelledby="editSupplierModalLabel" aria-hidden="true" data-bs-backdrop="static">
-  <div class="modal-dialog">
-      <form action="{{route('accountname.edit')}}" method="post" id="edit_supplier_name">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editSupplierModalLabel">Edit Supplier</h5>
-                <!-- Close icon -->
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div id="editsuccessMessage"></div>
-            <div  id="editerrorMessage" ></div>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editSupplierModalLabel">Edit Supplier</h5>
+                    <!-- Close icon -->
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div id="editsuccessMessage"></div>
+                <div  id="editerrorMessage" ></div>
                 <div class="modal-body">
-                    <div class="form-group">
-                    @csrf
-                    <input type="hidden" id="supplier_id" name="supplier_id">
-                    <div class="modal_input_wrap">
-                        <label>First Name</label>
-                        <input type="text" class="form-control" name="first_name" id="first_name" value="">
-                    </div>
-                    <div class="modal_input_wrap">
-                        <label>Last Name</label>
-                        <input type="text" class="form-control" name="last_name" id="last_name" value="">
-                    </div>
-                    <div class="modal_input_wrap">
-                        <label>Email</label>
-                        <input type="text" class="form-control" name="email" id="email" value="">
-                    </div>
-                    <div class="modal_input_wrap">
-                        <label>Phone</label>
-                        <input type="text" class="form-control" name="phone" id="phone" value="">
-                    </div>
-                    <div class="modal_input_wrap">
-                        <label for="" class="form-label">Status</label>
-                        <select class="form-control" name="status" id="status">
-                            <option value="1">Active</option>
-                            <option value="0">In-Active</option>
-                        </select>
-                    </div>
+                    <form action="{{route('accountname.edit')}}" method="post" id="edit_supplier_name">
+                        @csrf
+                        <div class="form-group">
+                            <input type="hidden" id="supplier_id" name="supplier_id">
+                            <div class="modal_input_wrap">
+                                <label>First Name</label>
+                                <input type="text" class="form-control" name="first_name" id="first_name" value="">
+                            </div>
+                            <div class="modal_input_wrap">
+                                <label>Last Name</label>
+                                <input type="text" class="form-control" name="last_name" id="last_name" value="">
+                            </div>
+                            <div class="modal_input_wrap">
+                                <label>Email</label>
+                                <input type="text" class="form-control" name="email" id="email" value="">
+                            </div>
+                            <div class="modal_input_wrap">
+                                <label>Phone</label>
+                                <input type="text" class="form-control" name="phone" id="phone" value="">
+                            </div>
+                            <div class="modal_input_wrap">
+                                <label for="" class="form-label">Status</label>
+                                <select class="form-select" name="status" id="status">
+                                    <option value="1">Active</option>
+                                    <option value="0">In-Active</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>   
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Save changes</button>
-            </div>
-            </form>   
         </div>
     </div>
-</div>
     <script>
         $(document).ready(function() {
-        var myModal = document.getElementById('editSupplierModal');
-        myModal.addEventListener('show.bs.modal', function (event) {
-            var button = event.relatedTarget; // Button that triggered the modal
+        document.getElementById('editSupplierModal').addEventListener('show.bs.modal', function (event) {
             // Set the value of the input element
-            document.getElementById('first_name').value = button.getAttribute('data-firt_name');
-            document.getElementById('last_name').value = button.getAttribute('data-last_name');
-            document.getElementById('phone').value = button.getAttribute('data-phone');
-            document.getElementById('email').value = button.getAttribute('data-email');
-            document.getElementById('supplier_id').value = button.getAttribute('data-id');
-            if (button.getAttribute('data-status') == 1) {
+            document.getElementById('phone').value = event.relatedTarget.getAttribute('data-phone');
+            document.getElementById('email').value = event.relatedTarget.getAttribute('data-email');
+            document.getElementById('supplier_id').value = event.relatedTarget.getAttribute('data-id');
+            document.getElementById('last_name').value = event.relatedTarget.getAttribute('data-last_name');
+            document.getElementById('first_name').value = event.relatedTarget.getAttribute('data-first_name');
+
+            if (event.relatedTarget.getAttribute('data-status') == 1) {
                 document.getElementById('status').options[0].selected = true;
             } else {
                 document.getElementById('status').options[1].selected = true;
@@ -80,11 +79,10 @@
 
         $("#edit_supplier_name").on('submit', function (e){
             e.preventDefault();
-            var formData = new FormData($('#edit_supplier_name')[0]);
             $.ajax({
                 type: 'POST',
                 url: '{{ route("supplier.edit") }}', // Replace with your actual route name
-                data: formData,
+                data: new FormData($('#edit_supplier_name')[0]),
                 processData: false,
                 contentType: false,
                 success: function(response) {
@@ -109,14 +107,11 @@
                         $('#successMessages').html('');
                         $('#editsuccessMessage').append('<div class="alert alert-success alert-dismissible fade show" role="alert">' + response.success + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
                         location.reload();
-                        // accountTable.ajax.reload();   
                     }
                 },
                 error: function(xhr, status, error) {
                     // Handle error response
-                    // console.error(xhr.responseText);
-                    const errorresponse = JSON.parse(xhr.responseText);
-                    $('#editerrorMessage').text(errorresponse.error);
+                    $('#editerrorMessage').text(JSON.parse(xhr.responseText).error);
                     $('#editerrorMessage').css('display','block');
                     setTimeout(function () {
                         $('#editerrorMessage').fadeOut();
@@ -125,28 +120,21 @@
             });
         });
 
-        $(document).ready(function() {
-        var supplierTable = $('#supplier_data').DataTable({
-                "paging": true,   // Enable pagination
-                "ordering": true, // Enable sorting
-                "searching": true, // Enable search
-                "pageLength": 40,
-                "lengthChange":false,
-                "data": <?php if(isset($data)){echo $data;}  ?>,
-                "columns": [
-                    { title: 'Supplier Name' },
-                    { title: 'Name' },
-                    { title: 'Email' },
-                    { title: 'Phone' },
-                    { title: 'Status' },
-                    { title: 'Action' },
-                ]
-            });
-            if (supplierTable.data().count() > 40) {
-                $('#supplier_data_paginate').show(); // Enable pagination
-            } else {
-                $('#supplier_data_paginate').hide();
-            }
+        $('#supplier_data').DataTable({
+            "paging": true,   // Enable pagination
+            "ordering": true, // Enable sorting
+            "searching": true, // Enable search
+            "pageLength": 40,
+            "lengthChange":false,
+            "data": <?php if(isset($data)){echo $data;}  ?>,
+            "columns": [
+                { title: 'Supplier Name' },
+                { title: 'Name' },
+                { title: 'Email' },
+                { title: 'Phone' },
+                { title: 'Status' },
+                { title: 'Action' },
+            ]
         });
     });
     </script>
