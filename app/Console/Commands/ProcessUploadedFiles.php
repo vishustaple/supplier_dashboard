8ad49796412cec7b530cc1858e129a7e000e2faa
vishustaple <vishustaple.in@gmail.com>
2024-04-25 11:28:20 +0530

    worked on issues

--- app/Console/Commands/ProcessUploadedFiles.php
+++ app/Console/Commands/ProcessUploadedFiles.php
@@ -744,7 +744,7 @@ class ProcessUploadedFiles extends Command
                                                         'created_by' => $fileValue->created_by,
                                                         'supplier_id' => $fileValue->supplier_id,
                                                         'amount' => (isset($keyAmount) && !empty($row[$keyAmount])) ? ((($row[$keyAmount] >= 0)) ? ($row[$keyAmount]) : (($fileValue->supplier_id == 1) ? (ltrim($row[$keyAmount], '-')) : ('0.0'))) : ('0.0'),
-                                                        'negative_amount' => (($row[$keyAmount] >= 0) && ($fileValue->supplier_id != 1)) ? ($row[$keyAmount]) : ('0.0'),
+                                                        'negative_amount' => (($row[$keyAmount] < 0) && ($fileValue->supplier_id != 1)) ? ($row[$keyAmount]) : ('0.0'),
                                                         'date' => (isset($keyInvoiceDate) && !empty($row[$keyInvoiceDate])) ? (Carbon::createFromTimestamp(ExcelDate::excelToTimestamp($row[$keyInvoiceDate]))->format('Y-m-d H:i:s')) : ($fileValue->start_date),
                                                         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                                                         'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
