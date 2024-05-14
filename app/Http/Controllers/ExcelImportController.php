f19819168b1258b3101b3c2f7e95c833a2288d7e
vishustaple <vishustaple.in@gmail.com>
2024-05-14 13:05:43 +0530

    worked on user delete

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -47,11 +47,17 @@ class ExcelImportController extends Controller
                 $cronString = 'Deleted';
             }
             
+            $userName = '';
+            if ($item->createdByUser) {
+                $userName = $item->createdByUser->first_name ?? '';
+                $userName .= ' ' . ($item->createdByUser->last_name ?? '');
+            }
+
             $formattedData[] = [
                 getSupplierName($item->supplier_id),
                 '<div class="file_td">'.$item->file_name.'</div>',
                 $cronString,
-                ((isset($item->createdByUser->first_name)) ? ($item->createdByUser->first_name) : ('')).' '.((isset($item->createdByUser->last_name)) ? ($item->createdByUser->last_name) : ('')),
+                $userName,
                 $item->created_at->format('m/d/Y'),
                 (isset($item->delete) && !empty($item->delete)) ? ('<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>') : ((isset($item->deleted_at) && !empty($item->deleted_at) ? '<button class="btn btn-danger btn-xs remove invisible" ><i class="fa-solid fa-trash"></i></button>' : '<button data-id="'.$item->id.'" class="btn btn-danger btn-xs remove" title="Remove File"><i class="fa-solid fa-trash"></i></button>')),
             ];
