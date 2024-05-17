14bf50056fe74903b629942bea549fc8171c2597
vishustaple <vishustaple.in@gmail.com>
2024-05-17 14:02:45 +0530

    worked on cron

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -172,12 +172,12 @@ class Order extends Model
             }
 
             if ($filter['core'] == 1) {
-                $query->orWhereNotIn('transaction_source_system_desc', ['Staples Promotional Products USA', 'Staples Technology Solutions']);
+                $query->orWhereNotIn('transaction_source_system_desc', ['Staples Promotional Products USA', 'Staples Technology Solutions', 'Sunrise']);
                 $query->orWhereNotIn('primary_product_hierarchy_desc', ['STS Technology', 'Promo']);
                 $query->where('on_contract_id', '=', 'N');
             } else {
-                $query->orWhereIn('transaction_source_system_desc', ['Staples Promotional Products USA', 'Staples Technology Solutions']);
-                $query->orWhereIn('transaction_source_system_desc', ['Sunrise']);
+                $query->orWhereIn('transaction_source_system_desc', ['Staples Promotional Products USA', 'Staples Technology Solutions', 'Sunrise']);
+                // $query->orWhereIn('transaction_source_system_desc', ['Sunrise']);
                 $query->orWhere('on_contract_id', '=', 'Y');
                 $query->orWhereIn('primary_product_hierarchy_desc', ['STS Technology', 'Promo']);
             }
