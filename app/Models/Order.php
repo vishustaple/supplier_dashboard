60097697bf4e9867c6dc3f41e40328066a9284f7
vishustaple <vishustaple.in@gmail.com>
2024-05-20 17:10:50 +0530

    worked on business report

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -242,11 +242,15 @@ class Order extends Model
             }
 
             if ($filter['core'] == 2) {
-                $query->where('price_method', 'LIKE', 'PPL%');
-                $query->where('price_method', 'LIKE', 'CTL%');
+                $query->where(function($query) {
+                    $query->orWhere('price_method', 'LIKE', 'PPL%')
+                        ->orWhere('price_method', 'LIKE', 'CTL%');
+                });
             } else {
-                $query->where('price_method', 'NOT LIKE', 'PPL%');
-                $query->where('price_method', 'NOT LIKE', 'CTL%');
+                $query->where(function($query) {
+                    $query->orWhere('price_method', 'NOT LIKE', 'PPL%')
+                        ->orWhere('price_method', 'NOT LIKE', 'CTL%');
+                });
             } 
 
             $query->orderBy('total_spend', 'desc')->limit(100);
