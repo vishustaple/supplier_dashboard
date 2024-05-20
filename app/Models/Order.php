92ab211e758908add468284e2dda0a2ef24c4b60
vishustaple <vishustaple.in@gmail.com>
2024-05-20 17:48:26 +0530

    worked on business report

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -217,7 +217,7 @@ class Order extends Model
 
             $query->orderBy('total_spend', 'desc')->limit(100);
             $queryData2 = $query->get()->toArray();
-            dd($query->toSql(), $query->getBindings());
+            // dd($query->toSql(), $query->getBindings());
         } else {
             $queryData2 = [];
         }
