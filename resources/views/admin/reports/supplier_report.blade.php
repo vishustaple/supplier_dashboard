48792a76f6d20eca4097a5f60fdab65a048fc7e9
vishustaple <vishustaple.in@gmail.com>
2024-05-23 16:25:02 +0530

    worked on changes

--- resources/views/admin/reports/supplier_report.blade.php
+++ resources/views/admin/reports/supplier_report.blade.php
@@ -116,14 +116,16 @@
         });
 
         $('#enddate').daterangepicker({
+            autoApply: true,
             showDropdowns: true,
+            showCustomRangeLabel: true,
             minYear: moment().subtract(7, 'years').year(),
-            maxYear: moment().year(),
+            maxDate: moment(),
             ranges: {
                 'Last Quarter': [moment().subtract(3, 'month').startOf('quarter'), moment().subtract(3, 'month').endOf('quarter')],
                 'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
                 'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
-                'Last 6 Months': [moment().subtract(6, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                'Last 6 Months': [moment().subtract(6, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
             }
         });
 
