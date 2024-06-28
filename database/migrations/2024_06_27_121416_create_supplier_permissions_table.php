f3049b9778ef3e1aa187ac35f6d871375c26933c
vishustaple <vishustaple.in@gmail.com>
2024-06-28 13:20:26 +0530

    worked on supplier show

--- database/migrations/2024_06_27_121416_create_supplier_permissions_table.php
+++ database/migrations/2024_06_27_121416_create_supplier_permissions_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('supplier_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_supplier_id')->constrained()->onDelete('cascade');
-            $table->foreignId('show_permissions_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_supplier_id');
+            $table->foreignId('show_permissions_id');
             $table->timestamps();
 
             // Manually define foreign key constraints with unique names
-            $table->foreign('category_supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
-            $table->foreign('show_permissions_id')->references('id')->on('show_permissions')->onDelete('cascade');
+            $table->foreign('category_supplier_id')->references('id')->on('suppliers');
+            $table->foreign('show_permissions_id')->references('id')->on('show_permissions');
         });
     }
 
