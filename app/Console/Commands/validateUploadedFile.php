534441b01b80f476b9d86eebc30aee76a88545ef
vishustaple <vishustaple.in@gmail.com>
2024-07-08 13:17:52 +0530

    worked on file validation

--- app/Console/Commands/validateUploadedFile.php
+++ app/Console/Commands/validateUploadedFile.php
@@ -173,8 +173,6 @@ class validateUploadedFile extends Command
                         })->where('supplier_id', $fileValue->supplier_id);
                 
                         if ($fileExist->count() > 0) {
-                            dd($fileExist->count());
-
                             /** Update cron two means start processing data into excel */
                             DB::table('uploaded_files')
                             ->where('id', $fileValue->id)
