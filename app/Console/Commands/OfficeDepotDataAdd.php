02c0aefc3843871252900be73415421b2f19695b
vishustaple <vishustaple.in@gmail.com>
2024-07-15 11:34:34 +0530

    worked on rebate report

--- app/Console/Commands/OfficeDepotDataAdd.php
+++ app/Console/Commands/OfficeDepotDataAdd.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Database\QueryException;
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use PhpOffice\PhpSpreadsheet\Reader\{Xls, Xlsx};
 use PhpOffice\PhpSpreadsheet\Shared\Date as ExcelDate;
 use App\Models\{
     Order,
@@ -34,168 +34,38 @@ class OfficeDepotDataAdd extends Command
      */
     public function handle()
     {
-        // $years = ['2020', '2021', '2022', '2023', '2024'];
-        // $filter['core'] = 1;
+        /** This is the folder path where we save the file */
+        $destinationPath = public_path('/excel_sheets');
 
-        // $query = DB::table('master_account_detail')->select('master_account_detail.account_name as account_name');
-        // $query->whereNotNull('master_account_detail.account_name');
-        // $query->groupBy('master_account_detail.account_name');
-        // $query->where('master_account_detail.category_supplier', 3);
-        // $account = $query->get();
-      
-        // foreach ($account as $accounts) {
-        //     foreach ($years as $year) {
-        //         $res[1] =[
-        //             'January',
-        //             'February',
-        //             'March',
-        //         ];
-        
-        //         $res[2] = [
-        //             'April',
-        //             'May',
-        //             'June',
-        //         ];
-        
-        //         $res[3] = [
-        //             'July',
-        //             'August',
-        //             'September',
-        //         ];
-        
-        //         $res[4] = [
-        //             'October',
-        //             'November',
-        //             'December',
-        //         ];
-        
-        //         $monthDates = [];
-        
-        //         for ($month = 1; $month <= 12; $month++) {
-        //             $start = date('Y-m-01', strtotime("$year-$month-01"));
-        //             $end = date('Y-m-t', strtotime("$year-$month-01"));
-            
-        //             $monthDates[] = [
-        //                 'start_date' => $start,
-        //                 'end_date' => $end,
-        //             ];
-        //         }
-        
-        //         $startDate1 = $monthDates[0]['start_date'];
-        //         $endDate1 = $monthDates[2]['end_date'];
-                
-        //         $startDate2 = $monthDates[3]['start_date'];
-        //         $endDate2 = $monthDates[5]['end_date'];
-                
-        //         $startDate3 = $monthDates[6]['start_date'];
-        //         $endDate3 = $monthDates[8]['end_date'];
-                
-        //         $startDate4 = $monthDates[9]['start_date'];
-        //         $endDate4 = $monthDates[11]['end_date'];
-        //         $query = Order::query() // Replace YourModel with the actual model you are using for the data
-        //         ->select('orders.id as id') 
-        //         ->leftJoin('order_product_details', 'orders.id', '=', 'order_product_details.order_id')
-        //         ->leftJoin('master_account_detail', 'orders.customer_number', '=', 'master_account_detail.account_number');
-        //         $query->whereIn('key', ['Core Flag']);
-    
-        //         if ($filter['core'] == 1) {
-        //             $query->whereIn('value', ['N', 'U']);
-        //         } else {
-        //             $query->whereIn('value', ['Y']);
-        //         }
-                
-        //         $query->whereYear('orders.date', $year);
-        //         $query->where('master_account_detail.account_name', $accounts->account_name);
-        //         $query->groupBy('order_product_details.order_id');
-        //         $query->orderBy('orders.amount', 'desc')->limit(100);
-    
-        //         $orderIdArray = [];
-    
-        //         if ($query->get()) {
-        //             foreach ($query->get() as $key => $value) {
-        //                 $orderIdArray[] = $value->id;
-        //             }
-        //         }
-                
-        //         if (!empty($orderIdArray)) {
-        //             $query1 = Order::query()
-        //             ->select(
-        //                 'orders.date as date',
-        //                 'orders.amount as total_spend',
-        //                 DB::raw("MAX(CASE WHEN `key` = 'UOM' THEN `value` ELSE NULL END) AS uom"),
-        //                 DB::raw("MAX(CASE WHEN `key` = 'SKU' THEN `value` ELSE NULL END) AS sku"),
-        //                 DB::raw("MAX(CASE WHEN `key` = 'Product Description' THEN `value` ELSE NULL END ) AS description"),
-        //                 DB::raw("MAX(CASE WHEN `key` = 'QTY Shipped' THEN `value` ELSE NULL END) AS quantity_purchased"),
-        //             )
-    
-        //             ->leftJoin('order_product_details', 'orders.id', '=', 'order_product_details.order_id')
-        //             ->whereIn('orders.id', $orderIdArray)
-        //             ->groupBy('order_product_details.order_id')
-        //             ->orderBy('orders.amount', 'desc');
-    
-        //             $queryData = $query1->get();
-        //             // dd($queryData);
-        //             $newFinalArray = [];
-        //             foreach ($queryData as $key => $value) {
-        //                 $query = DB::table('office_depot_order')->where('sku', $value->sku)
-        //                 ->selectRaw(
-        //                     'sku, uom, qty_shipped, product_description,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_net_price` ELSE 0 END) AS unit_price_q1_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_net_price` ELSE 0 END) AS unit_price_q2_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_net_price` ELSE 0 END) AS unit_price_q3_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_net_price` ELSE 0 END) AS unit_price_q4_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_web_price` ELSE 0 END) AS web_price_q1_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_web_price` ELSE 0 END) AS web_price_q2_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_web_price` ELSE 0 END) AS web_price_q3_price,
-        //                     SUM(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`unit_web_price` ELSE 0 END) AS web_price_q4_price,
-        //                     MIN(CASE WHEN `office_depot_order`.`shipped_date` BETWEEN ? AND ? THEN `office_depot_order`.`total_spend` ELSE 0 END) AS lowest_price',
-        //                     [$startDate1,$endDate1,$startDate2,$endDate2,$startDate3,$endDate3,$startDate4,$endDate4,$startDate1,$endDate1,$startDate2,$endDate2,$startDate3,$endDate3,$startDate4,$endDate4,$startDate1,$endDate4]
-        //                 );
-    
-        //                 if ($filter['core'] == 1) {
-        //                     $query->whereIn('core_flag', ['N', 'U']);
-        //                 } else {
-        //                     $query->whereIn('core_flag', ['Y']);
-        //                 }
-            
-        //                 $dataPrice = $query->first();
-        //                 $newFinalArray[] = [
-        //                     'supplier' => 3,
-        //                     'sku' => $value->sku,
-        //                     'account_name' => $accounts->account_name,
-        //                     'description' => $value->description,
-        //                     'product_type' => $filter['core'],
-        //                     'uom' => $value->uom,
-        //                     'category' => '',
-        //                     'quantity_purchased' => $value->quantity_purchased,
-        //                     'total_spend' => $value->total_spend,
-        //                     'unit_price_q1_price' => $dataPrice->unit_price_q1_price,
-        //                     'unit_price_q2_price' => $dataPrice->unit_price_q2_price,
-        //                     'unit_price_q3_price' => $dataPrice->unit_price_q3_price,
-        //                     'unit_price_q4_price' => $dataPrice->unit_price_q4_price,
-        //                     'web_price_q1_price' => $dataPrice->web_price_q1_price,
-        //                     'web_price_q2_price' => $dataPrice->web_price_q2_price,
-        //                     'web_price_q3_price' => $dataPrice->web_price_q3_price,
-        //                     'web_price_q4_price' => $dataPrice->web_price_q4_price,
-        //                     'lowest_price' => $dataPrice->lowest_price,
-        //                     'date' => $value->date,
-        //                     'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        //                     'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        //                 ];
-        //             }
-        //             DB::table('od_report')->insert($newFinalArray);
-        //         }
-        //     }
+        $inputFileType = \PhpOffice\PhpSpreadsheet\IOFactory::identify($destinationPath . '/test.xlsx');
 
-        $records = DB::table('staples_order')->select('id', 'shipped_date_id')->get();
-        // dd($records);
-        foreach ($records as $key => $value) {
-            if (!empty($value->shipped_date_id)) {
-                DB::table('staples_order')->where('id', $value->id)->update(['shipped_date_id' => Carbon::createFromTimestamp(ExcelDate::excelToTimestamp($value->shipped_date_id))->format('Y-m-d H:i:s')]);
-            }
+        if ($inputFileType === 'Xlsx') {
+            $reader = new Xlsx();
+        } elseif ($inputFileType === 'Xls') {
+            $reader = new Xls();
+        } else {
+            /** throw new Exception('Unsupported file type: ' . $inputFileType); */
         }
-        // 
-        // dd($newFinalArray);
 
+        /** Loading excel file using path and name of file from table "uploaded_file" */
+        $spreadSheet = $reader->load($destinationPath . '/test2.xlsx', 2);
+        $spreadSheet = $spreadSheet->getSheet(0)->toArray();
+
+        foreach ($spreadSheet as $key => $value) {
+            if ($key == 0) {
+                continue;
+            }
+
+            DB::table('wb_check_data')
+            ->insert([
+                'account' => $value[0],
+                'account_name' => $value[1],
+                'item_order' => $value[2],
+                'item_name' => $value[3],
+                'uom' => $value[4],
+                'qty' => $value[5],
+                'sum_of_sales' => $value[6],
+            ]);
+        }
     }
 }
