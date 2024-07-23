c1b65e6d67965b2f33f5ca0e61c70ffdc18e9137
vishustaple <vishustaple.in@gmail.com>
2024-07-23 18:40:28 +0530

    worked on consolidatedt repoert

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1336,7 +1336,7 @@ class Order extends Model
         if (isset($data) && !empty($data)) {
             $query->whereIn('order_id', $data);
         }
-        
+        dd($query->count());
         /** Getting result */
         $queryData = $query->get();
 
