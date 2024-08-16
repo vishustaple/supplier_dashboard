cada6dd035741ea999d28dfcdd4a7eaf467ade99
ankittesttest <ankit@staplelogic.in>
2024-08-16 18:30:18 +0530

    worked on report

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1506,13 +1506,39 @@ class Order extends Model
             ->join(DB::raw("(".$mediansQuery->toSql().") as m"), function($join) {
                 $join->on('m.year', '=', 'wa.year')
                     ->on('m.account_name', '=', 'wa.account_name');
-            })
-            ->selectRaw("
+            });
+
+            if ($csv) {
+                $query->selectRaw("
                 wa.year,
                 wa.account_name,
                 suppliers.supplier_name as supplier_name,
                 COALESCE(SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 52, 0) as average_week_52,
                 COALESCE(SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 10, 0) as average_week_10,
+                COALESCE(SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 2, 0) as average_week_2,
+                FORMAT(
+                    (
+                        (SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 52) -
+                        (SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 10) 
+                    ) / 
+                    (SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 52) * 100, 2
+                ) as gap_percentage,
+                m.median_52_weeks
+                ", [
+                    $start_date, $end_date,
+                    $start_date_10, $end_date_10,
+                    $start_date_2, $end_date_2,
+                    $start_date, $end_date,
+                    $start_date_10, $end_date_10,
+                    $start_date, $end_date
+                ]);
+            } else {
+                $query->selectRaw("
+                wa.year,
+                wa.account_name,
+                suppliers.supplier_name as supplier_name,
+                FORMAT(COALESCE(SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 52, 0), 2) as average_week_52,
+                FORMAT(COALESCE(SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 10, 0), 2) as average_week_10,
                 FORMAT(COALESCE(SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 2, 0), 2) as average_week_2,
                 FORMAT(
                     (
@@ -1522,15 +1548,17 @@ class Order extends Model
                     (SUM(CASE WHEN wa.date BETWEEN ? AND ? THEN wa.weekly_amount ELSE 0 END) / 52) * 100, 2
                 ) as gap_percentage,
                 m.median_52_weeks
-            ", [
-                $start_date, $end_date,
-                $start_date_10, $end_date_10,
-                $start_date_2, $end_date_2,
-                $start_date, $end_date,
-                $start_date_10, $end_date_10,
-                $start_date, $end_date
-            ])
-            ->groupBy('wa.year', 'wa.account_name', 'suppliers.supplier_name')
+                ", [
+                    $start_date, $end_date,
+                    $start_date_10, $end_date_10,
+                    $start_date_2, $end_date_2,
+                    $start_date, $end_date,
+                    $start_date_10, $end_date_10,
+                    $start_date, $end_date
+                ]);
+            }
+            
+            $query->groupBy('wa.year', 'wa.account_name', 'suppliers.supplier_name')
             ->havingRaw('CAST(average_week_52 AS DECIMAL(10, 2)) > CAST(average_week_10 AS DECIMAL(10, 2))')
             ->havingRaw('CAST(average_week_10 AS DECIMAL(10, 2)) != 0.00')
             ->mergeBindings($weeklyAmountsQuery->getQuery())
@@ -1561,7 +1589,7 @@ class Order extends Model
                     /** Prepare the final array for non-CSV */
                         $finalArray[$a]['account_name'] = $value->account_name;
                         $finalArray[$a]['supplier_name'] = $value->supplier_name;
-                        $finalArray[$a]['fifty_two_wk_avg'] = '$'.number_format($value->average_week_52, 2, '.', ',');
+                        $finalArray[$a]['fifty_two_wk_avg'] = '$'.$value->average_week_52;
                         $finalArray[$a]['ten_week_avg'] =  '$'.$value->average_week_10;
                         $finalArray[$a]['two_wk_avg_percentage'] = '$'.$value->average_week_2;
                         $finalArray[$a]['drop'] = $value->gap_percentage.'%';
