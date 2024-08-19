6c0e97df48c588531eb143bd17415ea8f78ceb79
ankittesttest <ankit@staplelogic.in>
2024-08-19 18:08:11 +0530

    worked on report

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1533,6 +1533,20 @@ class Order extends Model
                     'median' => $value->median,
                 ];
             } else {
+                $supplier = [1 => 'Grand & Toy', 2 => 'Grainger', 3 => 'Office Depot', 4 => 'Staples', 5 => 'WB Mason', 6 => 'Lyreco'];
+                $supplierDate = ''; 
+                foreach ($supplier as $key => $value) {
+                    if ($key ==  $filter['supplier']) {
+                        $date = Order::selectRaw("DATE_FORMAT(`date`, '%Y-%m-%d') as formatted_date")
+                        ->where('supplier_id', $key)
+                        ->orderBy('date', 'desc')
+                        ->limit(1)
+                        ->first();
+                        if ($date) {
+                            $supplierDate .= '<p class="card-text"><b>'.$value.': </b> '.$date->formatted_date.'</p>';
+                        }
+                    }
+                }
                 /** Prepare the final array for non-CSV */
                 $finalArray[$a]['account_name'] = $value->account_name;
                 $finalArray[$a]['supplier_name'] = $value->supplier_name;
