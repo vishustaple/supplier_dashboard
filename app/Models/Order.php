d69e7a06ace9cd30ad0d1a13fcfcd9db08123ead
ankittesttest <ankit@staplelogic.in>
2024-08-19 18:11:02 +0530

    worked on report

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1493,7 +1493,7 @@ class Order extends Model
                 $supplier = [1 => 'Grand & Toy', 2 => 'Grainger', 3 => 'Office Depot', 4 => 'Staples', 5 => 'WB Mason', 6 => 'Lyreco'];
                 $supplierDate = ''; 
                 foreach ($supplier as $key => $value) {
-                    if ($key ==  $filter['supplier']) {
+                    if ($value ==  $filter['supplier']) {
                         $date = Order::selectRaw("DATE_FORMAT(`date`, '%Y-%m-%d') as formatted_date")
                         ->where('supplier_id', $key)
                         ->orderBy('date', 'desc')
