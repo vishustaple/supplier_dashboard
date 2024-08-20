569739fd9e5b07a97e1a9c9077f4bac8e2d85d99
vishustaple <vishustaple.in@gmail.com>
2024-08-20 16:29:21 +0530

    worked on report

--- resources/views/admin/reports/consolidated_report.blade.php
+++ resources/views/admin/reports/consolidated_report.blade.php
@@ -112,9 +112,9 @@
             $('#downloadButton').on('click', function(e) {
                 e.preventDefault();
                 // Get all checked checkboxes within the DataTable
-                var checkedValues = [],
-                selectedAccounts = [];
+                var checkedValues = [], selectedAccounts = [];
                 
+                // Selecting the account name and create the selectedAccounts array
                 $('#consolidated_supplier_data tbody tr').each(function() {
                     var checkbox = $(this).find('input[type="checkbox"]:checked');
                     if (checkbox.length > 0) {
@@ -124,19 +124,20 @@
                     }
                 });
     
+                // Selecting the supplier_id and create the supplier_id array
                 $('.checkboxs:checked').each(function() {
                     checkedValues.push($(this).val());
                 });
 
-                console.log(selectedAccounts);
-                console.log(checkedValues);
-
+                // Added validation of empty supplier_id array
                 if (checkedValues.length === 0) {
                     $('#error').append('<svg xmlns="http://www.w3.org/2000/svg" style="display: none;"><symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></symbol></svg><div class="alert alert-danger alert-dismissible fade show" role="alert">  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg><strong>Error</strong> Please the supplier.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>');
                 } else {
+                    // Added validation of empty account name array
                     if (selectedAccounts.length === 0) {
                         $('#error').append('<svg xmlns="http://www.w3.org/2000/svg" style="display: none;"><symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></symbol></svg><div class="alert alert-danger alert-dismissible fade show" role="alert">  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg><strong>Error</strong> Please check the account.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>');
                     } else {
+                        // After checked the validation finally send ajax request
                         $.ajax({
                             url: '{{ route("consolidated-report.download") }}',
                             type: 'POST',
