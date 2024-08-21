581bfd2ef9192f598ce7d79ee5777ad00af45851
vishustaple <vishustaple.in@gmail.com>
2024-08-21 19:18:55 +0530

    worked on power bi reports

--- resources/views/admin/reports/consolidated_report.blade.php
+++ resources/views/admin/reports/consolidated_report.blade.php
@@ -137,6 +137,10 @@
                     if (selectedAccounts.length === 0) {
                         $('#error').append('<svg xmlns="http://www.w3.org/2000/svg" style="display: none;"><symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></symbol></svg><div class="alert alert-danger alert-dismissible fade show" role="alert">  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg><strong>Error</strong> Please check the account.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>');
                     } else {
+                        var button = document.getElementById('downloadButton'),
+                        formData = new FormData($('#import_form')[0]);
+                        button.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Loading';
+                        button.disabled = true;
                         // After checked the validation finally send ajax request
                         $.ajax({
                             url: '{{ route("consolidated-report.download") }}',
@@ -162,6 +166,8 @@
                                 ("0" + now.getSeconds()).slice(-2);
                                 link.download = 'Consolidated_Account_Report_' + dateStr + '.csv';
                                 link.click();
+                                button.disabled = false;
+                                button.innerHTML = '<i class="fa-solid me-1 fa-file-csv"></i>Download Selected Account Data';
                             },
                             error: function(xhr, status, error) {
                                 alert('File download failed!');
