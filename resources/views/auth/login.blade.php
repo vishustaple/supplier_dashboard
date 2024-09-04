3aaf8064fe8140a583042c400874671983a9b315
vishustaple <vishustaple.in@gmail.com>
2024-09-04 16:14:29 +0530

    worked on sso using microsft account

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,8 +42,8 @@
                                             <div class="text-center">
                                                 <button type="submit" class="btn blue ">Login</button>
                                             </div>
-                                            <div class="text-center">
-                                            <a href="{{ route('microsoft') }}" class="forgot-password-link">Power Bi Login</a>
+                                            <div class="text-center pt-2">
+                                                <a href="{{ route('microsoft') }}" class="btn blue" style="color:aliceblue; background-color:#b27929 !important">use microsoft account</a>
                                             </div>
                                             <div class="text-center mt-2">
                                                 <a href="{{route('user.forget')}}" class="forgot-password-link">Forgot Password?</a>
