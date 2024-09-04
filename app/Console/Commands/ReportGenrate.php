f0b01dcd7c09cf3b8f8efe08cf9e29d5e4fb864a
vishustaple <vishustaple.in@gmail.com>
2024-09-04 17:07:18 +0530

    worked on cahnges

--- app/Console/Commands/ReportGenrate.php
+++ app/Console/Commands/ReportGenrate.php
@@ -172,7 +172,7 @@ class ReportGenrate extends Command
 
                 $finalArray = [];
                 foreach ($queryData as $key => $value) {
-                    if ($value->percentage_drop >= 20 && $value->supplier_name == $values && $value->avg_52_weeks >= 500) {
+                    // if ($value->percentage_drop >= 20 && $value->supplier_name == $values && $value->avg_52_weeks >= 500) {
                         /** Prepare the final array for non-CSV */
                         $finalArray[] = [
                             'account_name' => $value->account_name,
@@ -186,7 +186,7 @@ class ReportGenrate extends Command
                             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
                         ];
-                    }
+                    // }
                 }
 
                 // dd($finalArray);
