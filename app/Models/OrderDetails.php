634ec402d5d79433a7236b2776e1bc1c061c5642
vishustaple <vishustaple.in@gmail.com>
2024-12-02 17:51:20 +0530

    worked on changes

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -16,36 +16,10 @@ class OrderDetails extends Model
      * @var array<int, string>
      */
     protected $fillable = [
+        'value',
         'order_id',
-        'created_by',
-        'invoice_date',
+        'created_at',
         'attachment_id',
-        'invoice_number',
-        'order_file_name',
+        'supplier_field_id',
     ];
-
-    public static function randomInvoiceNum($array = []) {
-        $min = 1000000000;  /** Minimum 10-digit number */
-        $max = 9999999999;  /** Maximum 10-digit number */
-        $number = mt_rand($min, $max);
-      
-        foreach ($array as $innerArray) {
-            foreach ($innerArray as $value) {
-                /** Check if the generated number already exists in the provided array */
-                if ($number == $value ) {
-                    /** If it exists, recursively call the function with the same array */
-                    $number = self::randomInvoiceNum($array);
-                }
-            }
-        }
-       
-        /** Check if the generated number already exists in the orders table */
-        $existingNumber = self::where('invoice_number', $number)->exists();
-        
-        if (!empty($existingNumber)) {
-            $number = self::randomInvoiceNum($array);        
-        }
-
-        return $number;
-    }
 }
