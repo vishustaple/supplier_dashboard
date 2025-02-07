d9ca70fd88d46a5bffe9eeb9e2a729fd83363b7d
vishustaple <vishustaple.in@gmail.com>
2025-02-07 15:37:18 +0530

    worked on commission

--- app/Console/Commands/RetrieveStaplesDiversityData.php
+++ app/Console/Commands/RetrieveStaplesDiversityData.php
@@ -29,37 +29,38 @@ class RetrieveStaplesDiversityData extends Command
      */
     public function handle()
     {
-        $sftpHost = env('SFTP_HOST');
-        $sftpUsername = env('SFTP_USERNAME');
-        $sftpPassword = env('SFTP_PASSWORD');
-        $sftpRemotePath = '/files/';
-        $localPath = storage_path('app/public/staples_data'); // Store in public
+        // $sftpHost = env('SFTP_HOST');
+        // $sftpUsername = env('SFTP_USERNAME');
+        // $sftpPassword = env('SFTP_PASSWORD');
+        // $sftpRemotePath = '/files/';
+        // $localPath = storage_path('app/public/staples_data'); // Store in public
 
-        $sftp = new SFTP($sftpHost);
-        if (!$sftp->login($sftpUsername, $sftpPassword)) {
-            $this->error('Failed to authenticate with SFTP server.');
-            return;
-        }
+        // $sftp = new SFTP($sftpHost);
+        // if (!$sftp->login($sftpUsername, $sftpPassword)) {
+        //     $this->error('Failed to authenticate with SFTP server.');
+        //     return;
+        // }
 
-        $files = $sftp->nlist($sftpRemotePath);
-        $downloadLinks = [];
+        // $files = $sftp->nlist($sftpRemotePath);
+        // $downloadLinks = [];
 
-        foreach ($files as $file) {
-            if ($file === '.' || $file === '..') continue;
+        // foreach ($files as $file) {
+        //     if ($file === '.' || $file === '..') continue;
 
-            $remoteFilePath = $sftpRemotePath . $file;
-            $localFilePath = $localPath . DIRECTORY_SEPARATOR . $file;
+        //     $remoteFilePath = $sftpRemotePath . $file;
+        //     $localFilePath = $localPath . DIRECTORY_SEPARATOR . $file;
 
-            if ($sftp->get($remoteFilePath, $localFilePath)) {
-                $this->info("Downloaded: $file");
-                // $sftp->delete($remoteFilePath);
-                print_r($file);
-                // Generate a public URL
-                $downloadLinks[] = asset("storage/staples_data/$file");
-            } else {
-                $this->error("Failed to download: $file");
-            }
-        }
+        //     if ($sftp->get($remoteFilePath, $localFilePath)) {
+        //         $this->info("Downloaded: $file");
+        //         // $sftp->delete($remoteFilePath);
+        //         print_r($file);
+        //         // Generate a public URL
+        //         $downloadLinks[] = asset("storage/staples_data/$file");
+        //     } else {
+        //         $this->error("Failed to download: $file");
+        //     }
+        // }
+        $downloadLinks = [0=>"sadsd",1=>"sdfdsf"];
         print_r($downloadLinks);
         if (!empty($downloadLinks)) {
             Mail::to('vishustaple.in@gmail.com')->send(new FileDownloaded($downloadLinks));
