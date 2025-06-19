0c85e12d3cd63295acf9036e0b409e837be36b78
vishustaple <vishustaple.in@gmail.com>
2025-06-19 15:54:00 +0530

    worked on account validation report rebate count issue and wbmelson report

--- resources/views/admin/query_filter.blade.php
+++ resources/views/admin/query_filter.blade.php
@@ -169,9 +169,9 @@
         var supplierDataTable = $('#supplier_report_data').DataTable({
             oLanguage: {sProcessing: '<div id="page-loader"><div id="page-loader-wrap"><div class="spinner-grow text-primary" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-success" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-danger" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-warning" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-info" role="status"><span class="sr-only">Loading...</span></div><div class="spinner-grow text-light" role="status"><span class="sr-only">Loading...</span></div></div></div>'},
             processing: true,
-            serverSide: false, 
+            serverSide: true, 
             lengthMenu: [40], // Specify the options you want to show
-            lengthChange: false, // Hide the "Show X entries" dropdown
+            lengthChange: true, // Hide the "Show X entries" dropdown
             searching:false, 
             pageLength: 40,
             ajax: {
